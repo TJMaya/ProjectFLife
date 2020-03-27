961843d62a9f823903e2cb36a3340e6e54b612cf
Jonathan Galon <jonjongalon@gmail.com>
2020-03-27 16:56:18 -0700

    gui stuff

--- src/GUI stuff/Inventory.gd
+++ src/GUI stuff/Inventory.gd
@@ -1,27 +1,17 @@
 extends Control
 var inv = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	pass 
 
 func _input(event):
 	if Input.is_key_pressed(KEY_I) and inv == false:
-		get_node(".").hide()
-		get_node("brownback").hide()
-		inv = true
-	elif Input.is_key_pressed(KEY_I) and inv == true:
 		get_node(".").show()
-		get_node("brownback").show()
+		inv = true
+		get_tree().paused = not get_tree().paused
+		
+	elif Input.is_key_pressed(KEY_I) and inv == true:
+		get_node(".").hide()
 		inv = false
-	pass # Replace with function body.
+		get_tree().paused = not get_tree().paused
+		
---
+++ src/GUI stuff/PauseMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+var pause = false
+
+func _ready():
+	pass 
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel") and pause == false:
+		get_node(".").show()
+		pause = true
+		get_tree().paused = not get_tree().paused
+		
+	elif event.is_action_pressed("ui_cancel") and pause == true:
+		get_node(".").hide()
+		pause = false
+		get_tree().paused = not get_tree().paused
+
+func _on_Button_pressed():
+	get_node(".").hide()
+	get_tree().paused = not get_tree().paused
+	pause = false
